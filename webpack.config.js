16276bc6d4eb6c05cccbdd6c32fb57b0a93afc8e
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-11-01 23:21:28 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue', '.json'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  devtool: '#eval-source-map'
+}
